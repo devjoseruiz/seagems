ea60e0d24f1c451abfb27a8fb4ef630cf6bb4660
José Ruiz <joseruiz@keemail.me>
2021-12-17 03:14:11 +0100

    Update assets paths

--- public/js/main.js
+++ public/js/main.js
@@ -12,16 +12,16 @@ GamePlayManager = {
     },
     preload: function(){
         // Load game assets
-        game.load.image("background", "../../assets/images/background.png")
-        game.load.image("explosion", "../../assets/images/explosion.png")
-        game.load.image("shark", "../../assets/images/shark.png")
-        game.load.image("fishes", "../../assets/images/fishes.png")
-        game.load.image("mollusk", "../../assets/images/mollusk.png")
-        game.load.image("bubble1", "../../assets/images/booble1.png")
-        game.load.image("bubble2", "../../assets/images/booble2.png")
+        game.load.image("background", "../assets/images/background.png")
+        game.load.image("explosion", "../assets/images/explosion.png")
+        game.load.image("shark", "../assets/images/shark.png")
+        game.load.image("fishes", "../assets/images/fishes.png")
+        game.load.image("mollusk", "../assets/images/mollusk.png")
+        game.load.image("bubble1", "../assets/images/booble1.png")
+        game.load.image("bubble2", "../assets/images/booble2.png")
 
-        game.load.spritesheet("horse", "../../assets/images/horse.png", 84, 156, 2)
-        game.load.spritesheet("jewells", "../../assets/images/diamonds.png", 81, 84, 4)
+        game.load.spritesheet("horse", "../assets/images/horse.png", 84, 156, 2)
+        game.load.spritesheet("jewells", "../assets/images/diamonds.png", 81, 84, 4)
     },
     create: function(){
         this.screenXCenter = game.width / 2
